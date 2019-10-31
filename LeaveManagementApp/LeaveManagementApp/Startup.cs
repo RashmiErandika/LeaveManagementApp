dd24e01c1fb45103ec895f2d6ed99592f7072b6f
Rashmi Erandika <Rashmi.Erandika@innovative-e.com>
2019-10-31 10:11:30 +0530

    Create UserRoles

--- LeaveManagementApp/LeaveManagementApp/Startup.cs
+++ LeaveManagementApp/LeaveManagementApp/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using LeaveManagementApp.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(LeaveManagementApp.Startup))]
@@ -9,6 +12,38 @@ namespace LeaveManagementApp
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateUserAndRoles();
+        }
+
+        public void CreateUserAndRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole("Admin");
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole("Employee");
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Supervisor1"))
+            {
+                var role = new IdentityRole("Supervisor1");
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Supervisor2"))
+            {
+                var role = new IdentityRole("Supervisor2");
+                roleManager.Create(role);
+            }
         }
     }
 }
